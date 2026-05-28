4f1504dd2c94776dd7749b2f2bf377de9525bfd5
karen_nasreddine <nasreddinekaren@gmail.com>
2026-05-28 15:16:08 +0300

    my first upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-import Pets from "./pages/Pets.jsx";
+import Pets from "./pages/Pets";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
